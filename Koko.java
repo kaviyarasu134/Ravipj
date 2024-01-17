4c8470c49191fe6772f12622b3c8f6bd50897ba9
kaviyarasu134 <kaviyarasuravi5@gmail.com>
2024-01-17 15:52:47 +0530

    mjjj

---
+++ Koko.java
@@ -0,0 +1,45 @@
+package org.kkk;
+
+public class Koko {
+	
+	
+	public Koko() {
+		
+		
+		System.out.println("default");
+	}
+	
+	private void koko(String name) {
+		
+		System.out.println("para");
+		
+		
+	}
+	
+	private void koko(int id) {
+		
+		System.out.println("int");
+
+
+	}
+	
+	public static void main(String[] args) {
+		
+	new Koko();
+	
+	
+	
+	
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
